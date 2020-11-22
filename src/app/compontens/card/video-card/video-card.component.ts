86070ec426c53b317cb2b89257ab4a7c369c4e52
mario robayo <marioferyhwh@gmail.com>
2020-11-22 16:36:25 -0500

    se llama sweetalert y se usa  para un modal

--- src/app/compontens/card/video-card/video-card.component.ts
+++ src/app/compontens/card/video-card/video-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { VideoModel } from "src/app/interfaces/answer-youtube.model";
+import Swal from "sweetalert2";
 
 @Component({
   selector: "app-video-card",
@@ -11,4 +12,31 @@ export class VideoCardComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  mostrarVideo() {
+    Swal.fire({
+      title: `<strong>${this.data.title}</strong>`,
+      //icon: "info",
+      html: `<iframe
+      width="100%"
+      height="315"
+      src="https://www.youtube.com/embed/${this.data.resourceId.videoId}"
+      frameborder="0"
+      allow="accelerometer;
+       autoplay;
+       clipboard-write;
+       encrypted-media;
+       gyroscope;
+       picture-in-picture"
+        allowfullscreen>
+      </iframe>`,
+      //showCloseButton: true,
+      //showCancelButton: true,
+      // focusConfirm: false,
+      //  confirmButtonText: '<i class="fa fa-thumbs-up"></i> Great!',
+      //  confirmButtonAriaLabel: "Thumbs up, great!",
+      // cancelButtonText: '<i class="fa fa-thumbs-down"></i>',
+      // cancelButtonAriaLabel: "Thumbs down",
+    });
+  }
 }
